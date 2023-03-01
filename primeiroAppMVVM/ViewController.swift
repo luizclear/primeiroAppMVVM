81ff6501fdc6d6c9bb6874b5228c1727d2660261
Luiz <clemente10claro@gmail.com>
2023-03-01 11:20:06 -0300

    Initial Commit

---
+++ primeiroAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  primeiroAppMVVM
+//
+//  Created by Luiz Felipe Clemente Claro on 01/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
